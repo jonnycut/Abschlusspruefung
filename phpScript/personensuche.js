b4847b93dc18b3d003c4820c523fb663fde0cffc
KNapret <jonnycut@yahoo.de>
2016-06-13 19:36:52 +0200

    php und ajaxübungen

--- phpScript/personensuche.js
+++ phpScript/personensuche.js
@@ -1,26 +1,37 @@
 "use strict"
 
+//HTML Elemente aus dem document holen
 var button = document.getElementById("starteSuche");
 var ausgabe = document.getElementById("ausgabe");
 
 
-
+//EventListener am Button registrieren
 button.addEventListener('click', function(e){
+    //bei Click das Value aus dem textfeld holen
     var  gesuchtePerson = document.getElementById("eingabe").value;
-    console.log("geht");
+
+    //Funktion aufrufen
     personenSuche(gesuchtePerson);
 });
 
 
-function personenSuche(name){
+function personenSuche(gesuchtePerson){
+    //neuen XMLhttpRequest erstellen
    let xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.open('GET','server.php?eingabe='+name,true);
+    //request öffnen, Methoe = GET, URL = server.php, ?=eingabe+gesuchtePerson -> Variablen übergeben, true -> Asynchroner Modus, Script läuft weiter
+    xmlhttp.open('GET','server.php?eingabe='+gesuchtePerson,true);
+
+    //Eventlistener am Request registrieren, sobald sich der readystate ändet, werden Befehle ausgeführt
     xmlhttp.addEventListener('readystatechange',function(){
         if(xmlhttp.readyState === 4 && xmlhttp.status === 200){
+            //Inhalt des Responsetextes in das div "ausgabe" schreiben
             ausgabe.innerHTML = xmlhttp.responseText;
         }
     });
+
+
+    //request absenden...----> WICHTIG!!
 
     xmlhttp.send();
 }
--- phpScript/server.php
+++ phpScript/server.php
@@ -6,6 +6,7 @@
  * Time: 17:59
  */
 
+/*TestArray mit Personen*/
 $personen = [
 
     'Kai' => [
@@ -29,15 +30,20 @@ $personen = [
     ]];
 
 
+/*Wenn Variable gesetzt und nicht "*" als Eingabe kommt*/
 if (isset($_GET['eingabe']) && $_GET['eingabe'] != "*") {
     $gesuchtePerson = $_GET['eingabe'];
+
+    /*Array durchjuckeln, wenn $Key = $gesuchtePerson, ReturnString mit HTML Tabelle bauen
+    mit $value hat man das Array der jeweiligen Person in der Hand, mit den entsprechenden
+    Schlüsseln ('alter','Familienstand' etc) kommt man dann an die Werte des Arrays*/
 
     foreach ($personen as $key => $value) {
         if ($key == $gesuchtePerson) {
             $tHead = "<table><thead><tr><th>Name </th><th>Alter </th><th>Familenstand </th><th>Wohnort </th></tr></thead><tbody>";
             $tInhalt = "<tr><td> " . $key . "</td><td> " . $value['alter'] . "</td><td> " . $value['Familienstand'] . "</td><td> " . $value['Wohnort'] . "</td></tr></tbody></table>";
 
-
+            /*Gebaute String zusammen zurückgeben, dies wird dann im script in die DIV Box geschrieben*/
             echo $tHead . $tInhalt;
             return;
         }
@@ -46,6 +52,8 @@ if (isset($_GET['eingabe']) && $_GET['eingabe'] != "*") {
     echo "Person nicht gefunden!";
 
 } elseif ($_GET['eingabe'] == "*") {
+    /*Bei eingabe = * werden alle Personen des Arrays in eine Tabelle geschrieben und diese als Ganzes returnt*/
+
     $tHead = $tHead = "<table><thead><tr><th>Name </th><th>Alter </th><th>Familenstand </th><th>Wohnort </th></tr></thead><tbody>";
     $tInhalt ="";
     foreach($personen as $key =>$value){
@@ -61,6 +69,7 @@ if (isset($_GET['eingabe']) && $_GET['eingabe'] != "*") {
 
 
 } else {
+
     echo "Fehler in der Übertragung";
     return;
 }
