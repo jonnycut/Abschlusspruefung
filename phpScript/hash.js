04b03d5f01a1b65902b8bf0b2a26b7348e1c0cd1
KNapret <jonnycut@yahoo.de>
2016-06-15 20:00:56 +0200

    HashCode php eimgefügt

---
+++ phpScript/hash.js
@@ -0,0 +1,30 @@
+"use strict"
+
+var textArea = document.getElementById("textArea");
+
+textArea.addEventListener('keypress', function(e){
+    console.log("Listener aktiviert")
+
+    let eingabeString = textArea.value;
+
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET','serverHash.php?eingabeString='+eingabeString,true);
+
+    xmlhttp.addEventListener('readystatechange',function(){
+
+        if(xmlhttp.readyState ===4 &&xmlhttp.status ===200 ){
+
+            document.getElementById("md5Ausgabe").innerHTML = xmlhttp.responseText.split(";")[0];
+            document.getElementById("sha1Ausgabe").innerHTML = xmlhttp.responseText.split(";")[1];
+            console.log(xmlhttp.responseText);
+
+        }
+
+    });
+
+    xmlhttp.send();
+
+
+
+
+});
---
+++ phpScript/serverHash.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: KNapret
+ * Date: 15.06.2016
+ * Time: 09:23
+ */
+
+
+if(isset($_GET['eingabeString'])){
+
+    $md5Hash = md5($_GET['eingabeString']);
+    $sha1Hash = sha1($_GET['eingabeString']);
+
+    echo $md5Hash.";".$sha1Hash;
+
+}
+
+?>
+
